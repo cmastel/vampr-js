dce0f03a0b325801e3a2a252104b63574238903e
Chris Mastel <chris.mastel@gmail.com>
2019-11-01 18:53:10 +0000

    completed initial problems

--- vampire.js
+++ vampire.js
@@ -10,22 +10,31 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberFromOriginal = 0;
+    let currentVampire = this;
+    while(currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberFromOriginal++;
+    }
+    return numberFromOriginal;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    const thisFromOriginal = this.numberOfVampiresFromOriginal;
+    const vampireFromOriginal = vampire.numberOfVampiresFromOriginal;
+    return (thisFromOriginal < vampireFromOriginal);
   }
 
   /** Stretch **/
@@ -36,6 +45,34 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+
+    // get an array containing path to root fo thisVampire
+    let currentVampire = this;
+    let thisVampirePath = [];
+    while(currentVampire.creator) {
+      thisVampirePath.push(currentVampire);
+      currentVampire = currentVampire.creator;
+    }
+    thisVampirePath.push(currentVampire); // final push to add last vampire
+
+    // get an array containing path to roo for vampire
+    currentVampire = vampire;
+    let vampireVampirePath = [];
+    while(currentVampire.creator) {
+      vampireVampirePath.push(currentVampire);
+      currentVampire = currentVampire.creator;
+    }
+    vampireVampirePath.push(currentVampire); // final push to add last vampire
+
+    // nested for loops to determine when along the path the names are the same
+    // this is the closestCommonAncestor, and the function returns that vampire
+    for (let mainVampire of thisVampirePath) {
+      for (let testVampire of vampireVampirePath) {
+        if (mainVampire.name === testVampire.name) {
+          return mainVampire;
+        }
+      }
+    }
 
   }
 }
